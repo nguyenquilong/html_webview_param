9e655b042e1dfe32f0047c1bbb7ba31e0628bdc4
nguyenquilong <35983478+nguyenquilong@users.noreply.github.com>
2020-10-02 07:38:31 +0700

    first up

---
+++ android/app/src/main/kotlin/com/example/webView/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webView
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:webView/src/webview_demo.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Demo Webview",
+    home: WebViewTest(),
+  ));
+}
